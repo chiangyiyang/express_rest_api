11d7143a1ec0ae89a15536d218244a71a38a6eba
Yiyang <chiangyiyang@gmail.com>
2019-12-06 00:35:07 +0800

    使用ID查詢資料集

--- app.js
+++ app.js
@@ -10,4 +10,15 @@ app.get("/items", (req, res) => {
   res.json(data);
 });
 
+app.get("/items/:id", (req, res) => {
+  const itemId = req.params.id;
+  const item = data.find(_item => _item.id === itemId);
+
+  if (item) {
+     res.json(item);
+  } else {
+     res.json({ message: `item ${itemId} doesn't exist`})
+  }
+});
+
 app.listen(3000, () => console.log('Listen on port 3000'));
