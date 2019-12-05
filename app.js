d4c1b8a585b8eb569b36b07aba14e2c9d70366da
Yiyang <chiangyiyang@gmail.com>
2019-12-06 01:47:26 +0800

    使用LevelDB儲存資料

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 
 const express = require('express');
 const body_parser = require('body-parser');
-const data = require('./fake_data');
+
+const levelup = require('levelup');
+const leveldown = require('leveldown');
+const encode = require('encoding-down')
+const db = levelup(encode(leveldown('./db'), { valueEncoding: 'json' }))
 
 const app = express();
 app.use(body_parser.json());
@@ -9,29 +13,47 @@ app.use(body_parser.json());
 app.get('/', (req, res) => res.send('Rest Api with Node and Express!'));
 
 app.get("/items", (req, res) => {
-  res.json(data);
+  db.get('items', function (err, value) {
+    if (err && err.notFound) return res.json({ message: 'No item' });
+    if (err) return console.log('Error!', err);
+    res.json(value);
+  });
 });
 
 app.get("/items/:id", (req, res) => {
-  const itemId = req.params.id;
-  const item = data.find(_item => _item.id === itemId);
+  db.get('items', function (err, value) {
+    if (err && err.notFound) return res.json({ message: 'No item' });
+    if (err) return console.log('Error!', err);
+    const itemId = req.params.id;
+    const item = value.find(_item => _item.id === itemId);
 
-  if (item) {
-     res.json(item);
-  } else {
-     res.json({ message: `item ${itemId} doesn't exist`})
-  }
+    if (item) {
+      res.json(item);
+    } else {
+      res.json({ message: `item ${itemId} doesn't exist` })
+    }
+  });
 });
 
 app.post("/items", (req, res) => {
-  const item = req.body;
-  console.log('Adding new item: ', item);
+  db.get('items', function (err, value) {
+    if (err && !err.notFound) return console.log('Error!', err);
+    if (err && err.notFound) value = [];
+    const item = req.body;
+    console.log('Adding new item: ', item);
+    console.log(value);
 
-  // add new item to array
-  data.push(item)
+    // add new item to array
+    value.push(item)
 
-  // return updated list
-  res.json(data);
+    // return updated list
+    res.json(value);
+
+    // save list
+    db.put('items', value, function (err) {
+      if (err) return console.log('Ooops!', err);
+    });
+  });
 });
 
 app.listen(3000, () => console.log('Listen on port 3000'));
